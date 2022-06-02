d778b5e5c0954ddc8be3207800e8be049b4eb1f0
olin ariel <arielo@wix.com>
2022-06-02 20:57:53 +0300

    iframe

--- loader.js
+++ loader.js
@@ -1,13 +1,21 @@
-const onPrototypeReady = () => {
+const onPrototypeReady = (src) => {
   const body = document.querySelector("body");
-  body.classList.add("loading");
-  body.insertAdjacentHTML("afterbegin", ` <span class="loader"></span>`);
 
-  window.addEventListener("load", function (event) {
+  if (!src) {
+    body.classList.add("loading");
+    body.insertAdjacentHTML("afterbegin", `<span class="loader"></span>`);
+  } else {
+    body.insertAdjacentHTML(
+      "afterbegin",
+      `<iframe src="https://www.w3schools.com" title="W3Schools Free Online Web Tutorials"></iframe>
+    `
+    );
+  }
+  window.addEventListener("load", function () {
     prototypeFunc();
     body.classList.remove("loading");
     document.querySelector(".loader")?.remove();
     body.classList.add("loaded");
   });
 };
-onPrototypeReady();
+onPrototypeReady("https://www.google.com");
